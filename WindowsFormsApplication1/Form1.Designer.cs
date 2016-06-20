4c52ef815b579e950b0251ce4f03eeb9c8b1f092
Bimal Varghese <bimal.varghese@ADRAPIDVALUE.COM>
2016-06-20 16:21:13 +0530

    Impemented initial requirements

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnData = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.pictureBoxThumbnail = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(224, 24);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(13, 27);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(205, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(339, 23);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(75, 23);
+            this.btnData.TabIndex = 2;
+            this.btnData.Text = "Get Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(13, 93);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(205, 157);
+            this.txtResult.TabIndex = 3;
+            // 
+            // pictureBoxThumbnail
+            // 
+            this.pictureBoxThumbnail.Location = new System.Drawing.Point(251, 103);
+            this.pictureBoxThumbnail.Name = "pictureBoxThumbnail";
+            this.pictureBoxThumbnail.Size = new System.Drawing.Size(139, 107);
+            this.pictureBoxThumbnail.TabIndex = 4;
+            this.pictureBoxThumbnail.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 262);
+            this.Controls.Add(this.pictureBoxThumbnail);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnData);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.PictureBox pictureBoxThumbnail;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExifLib;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog { FileName = txtFile.Text, Filter = "JPEG Images (*.jpg)|*.jpg" };
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = dlg.FileName;
+            }
+        }
+
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Install-Package ExifLib
+                //http://www.codeproject.com/Articles/36342/ExifLib-A-Fast-Exif-Data-Extractor-for-NET-2-0
+
+                txtResult.Clear();
+                using (var reader = new ExifReader(txtFile.Text))
+                {
+                    var thumbNailBytes = reader.GetJpegThumbnailBytes();
+
+                    if (thumbNailBytes == null)
+                        pictureBoxThumbnail.Image = null;
+                    else
+                    {
+                        using (var stream = new MemoryStream(thumbNailBytes))
+                            pictureBoxThumbnail.Image = Image.FromStream(stream);
+                    }
+
+                    double[] gpsLatitude;
+                    string output = string.Empty;
+
+                    if (reader.GetTagValue(ExifTags.GPSLatitude, out gpsLatitude))
+                    {
+                        output = string.Format("{0}: {1},{2},{3} ", "GPS Latitude", gpsLatitude[0], gpsLatitude[1], gpsLatitude[2]);
+
+                        txtResult.AppendText(output);
+                    }
+
+                    double[] gpsLongitude;
+
+                    if (reader.GetTagValue(ExifTags.GPSLongitude, out gpsLongitude))
+                    {
+
+                        output = string.Format("{0}: {1},{2},{3} ", "GPS Longitude", gpsLongitude[0], gpsLongitude[1], gpsLongitude[2]);
+
+                        txtResult.AppendText(Environment.NewLine);
+                        txtResult.AppendText(output);
+                    }
+
+                    DateTime datePictureTaken;
+                    if (reader.GetTagValue(ExifTags.DateTimeOriginal, out datePictureTaken))
+                    {
+                        txtResult.AppendText(Environment.NewLine);
+                        txtResult.AppendText(string.Format("{0}: {1} ", "Date ", datePictureTaken.ToShortDateString()));
+                    }
+
+                    string locationName = string.Empty;
+                    if (gpsLatitude != null && gpsLongitude != null)
+                    {
+                        locationName = GetLocationName(gpsLatitude[2], gpsLongitude[2]);
+
+                        txtResult.AppendText(Environment.NewLine);
+                        txtResult.AppendText(string.Format("{0}: {1} ", "Location ", locationName));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public string GetLocationName(double latitude, double longitude)
+
+        {
+            string locationName = string.Empty;
+
+            //string baseUri = "https://maps.googleapis.com/maps/api/timezone/json?location={0},{1}&timestamp=1374868635&sensor=false";
+
+            string baseUri = "http://dev.virtualearth.net/REST/v1/Locations/{0},{1}?key=AjzHTEndVFIIdMNhX3hDjsbfLfZPY3qU_OLueRyfZ-L9dhDxRArlhCMWRDoYcAmA";
+
+            string requestUri = string.Format(baseUri, latitude, longitude);
+
+            using (WebClient wc = new WebClient())
+            {
+                string result = wc.DownloadString(requestUri);
+
+                var apiResult = JsonConvert.DeserializeObject<dynamic>(result);
+
+                //locationName = apiResult.timeZoneId;
+                //locationName = locationName.Split('/')[1];
+
+                locationName = apiResult.resourceSets[0].resources[0].name;
+            }
+
+            return locationName;
+        }
+    }
+}
